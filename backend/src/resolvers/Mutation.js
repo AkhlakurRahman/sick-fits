1933434d0811e493e07bd89a00dbf3a3747ce869
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-09-06 15:29:38 +0600

    createItem backend logic completed and frontend query and mutation working

---
+++ backend/src/resolvers/Mutation.js
@@ -0,0 +1,19 @@
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO check if user is logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    console.l;
+
+    return item;
+  }
+};
+
+module.exports = Mutations;
